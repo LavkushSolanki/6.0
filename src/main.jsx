e506665662fbfe242d2333c324091e55831a8408
Ayushmanshaw <ayushmanshaw6471@gmail.com>
2024-10-16 12:39:41 +0530

    darker background

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ createRoot(document.getElementById("root")).render(
         cursor: "url(/assets/images/logo/gunf.png), auto",
         // background:
         //   "linear-gradient(90deg, rgba(201, 9, 32, 1) 0%, rgba(11, 11, 11, 1) 89%)",
-        backgroundColor: "rgba(0,0,0,0.9)",
+        backgroundColor: "rgba(0,0,0,1)",
         fontFamily: "Anton SC, sans-serif",
         fontWeight: 100,
         fontSize: "large",
