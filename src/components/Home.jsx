615d652e68792bc55a6952819cc2b4b40f2e6609
Ayushmanshaw <ayushmanshaw6471@gmail.com>
2024-10-18 11:08:51 +0530

    change home.jsx file

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Home.css'; // Make sure to update your CSS
 import ParticlesComponent from "./Particles"; // Import the Particles component
-import Home_Center from "./Home_Center"
+import Home_Center from "./Home_center"
 
 const Home = () => {
   return (
